6e81d2801e7d831df3957128b50add9e31c0a693
himu23 <hmshete12@gmail.com>
2024-06-20 10:18:46 +0530

    commit

---
+++ codechef/completecppcourse/arraysstringssorting/sizeof.cpp
@@ -0,0 +1,5 @@
+/*
+    The result show 20 instead of 5, when the array contains 5 elements.
+    It is because the sizeof() operator returns the size of a type in bytes
+    int type is usually 4 bytes, so from the example above, 4 x 5 (4 bytes x 5 elements) = 20 bytes.
+*/
