783720d6cd2dc47d28854abb3defa0bc3c5ff939
himu23 <hmshete12@gmail.com>
2024-06-21 08:01:03 +0530

    commit

---
+++ codechef/OOP/introtooop/problem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class User{
+private:
+    string name="Alice";
+    string password="Wonderland";
+
+public:
+    void set(string name,string password){
+        this->name = name;
+        this->password = password;
+    }
+    void get(){
+        cout<<name<<" "<<password<<endl;
+    }
+};
+
+int main() {
+    User obj;
+    obj.get();
+    obj.set("Tom","Jerry");
+    obj.get();
+    return 0;
+}
+//"this" is used within a class member function to refer to the object on which the member function is called.
--- codechef/completecppcourse/arraysstringssorting/sizeof.cpp
+++ codechef/completecppcourse/arraysstringssorting/sizeof.cpp
@@ -3,3 +3,6 @@
     It is because the sizeof() operator returns the size of a type in bytes
     int type is usually 4 bytes, so from the example above, 4 x 5 (4 bytes x 5 elements) = 20 bytes.
 */
+string 24
+int 4
+the sizeof object is the sum of all individual data members.
