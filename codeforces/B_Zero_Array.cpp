e67c2c8a8efa74165ba4f5c4333a662d2683b248
himu23 <hmshete12@gmail.com>
2025-07-20 18:41:23 +0530

    commit

--- codeforces/B_Zero_Array.cpp
+++ codeforces/B_Zero_Array.cpp
@@ -1,3 +1,95 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// void dbg_out() { cerr << endl; }
+// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// #ifdef LOCAL
+// #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+// #else
+// #define dbg(...)
+// #endif
+
+// #define ar array
+// #define ll long long
+// #define ld long double
+// #define sza(x) ((int)x.size())
+// #define all(a) (a).begin(), (a).end()
+
+// const int MAX_N = 1e5 + 5;
+// const ll MOD = 1e9 + 7;
+// const ll INF = 1e9;
+// const ld EPS = 1e-9;
+
+// // Custom hash for unordered_map/set
+// struct custom_hash {
+//     static uint64_t splitmix64(uint64_t x) {
+//         x += 0x9e3779b97f4a7c15;
+//         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+//         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+//         return x ^ (x >> 31);
+//     }
+//     size_t operator()(uint64_t x) const {
+//         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+//         return splitmix64(x + FIXED_RANDOM);
+//     }
+//     template<typename T, typename U>
+//     size_t operator()(const pair<T, U>& p) const {
+//         uint64_t h1 = operator()(static_cast<uint64_t>(p.first));
+//         uint64_t h2 = operator()(static_cast<uint64_t>(p.second));
+//         return h1 ^ (h2 << 1);
+//     }
+// };
+// template<typename K, typename V>
+// using safe_umap = unordered_map<K, V, custom_hash>;
+// template<typename T>
+// using safe_uset = unordered_set<T, custom_hash>;
+
+
+
+// void solve() {
+//     ll n; cin>>n;
+//     multiset<ll> a;
+//     for(ll i=0;i<n;i++){
+//         ll temp; cin>>temp;
+//         a.insert(temp);
+//     }
+//     while(a.size()>=2 && *next(a.rbegin())>0){
+//         auto it1=a.rbegin();
+//         auto it2=next(a.rbegin());
+//         ll lar=*it1;
+//         ll sec=*it2;
+//         a.erase(a.find(lar));
+//         a.erase(a.find(sec));
+//         a.insert(lar-sec);
+//         a.insert(0);
+//     }
+//     auto it=a.rbegin();
+//     if(*it==0){
+//         cout<<"YES"<<endl;
+//     }
+//     else{
+//         cout<<"NO"<<endl;
+//     }
+//     return;
+    
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0); cout.tie(0);
+//     int tc = 1;
+//     //cin >> tc;
+//     for (int t = 1; t <= tc; t++) {
+//         // cout << "Case #" << t << ": ";
+//         solve();
+//     }
+// }
+
+//it worked on the given testcases but its totally flawed logic. like thinkof something like 10 10 10 10 10 above will give no but the ans is yes
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -50,12 +142,21 @@ using safe_uset = unordered_set<T, custom_hash>;
 
 
 void solve() {
-    int n; cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++){
-       cin>>a[i];
+    ll n; cin>>n;
+    ll sum=0;
+    ll lar=0;
+    vector<ll> a(n);
+    for(ll i=0;i<n;i++){
+        cin>>a[i];
+        sum+=a[i];
+        lar=max(lar,a[i]);
     }
-    
+    if(sum%2!=0){
+        cout<<"NO"<<endl;
+        return;
+    }
+    if(lar>sum/2) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
 }
 
 int main() {
