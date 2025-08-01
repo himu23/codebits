1ca0c9862aa38dcede2d57c02516732515febb31
himu23 <hmshete12@gmail.com>
2025-08-01 14:12:13 +0530

    commit

---
+++ cses/sorting and searching/Concert_Tickets.cpp
@@ -0,0 +1,206 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// void dbg_out() { cerr << endl; }
+// template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// #ifdef LOCAL
+// #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+// #else
+// #define dbg(...)
+// #endif
+
+// #define ar array
+// #define ll long long
+// #define ld long double
+// #define sza(x) ((int)x.size())
+// #define all(a) (a).begin(), (a).end()
+// #define pb push_back
+// #define umap unordered_map
+
+// const int MAX_N = 1e5 + 5;
+// const ll MOD = 1e9 + 7;
+// const ll INF = 1e9;
+// const ld EPS = 1e-9;
+
+// // Custom hash for unordered_map/set
+// struct custom_hash {
+//     static uint64_t splitmix64(uint64_t x) {
+//         x += 0x9e3779b97f4a7c15;
+//         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+//         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+//         return x ^ (x >> 31);
+//     }
+//     size_t operator()(uint64_t x) const {
+//         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+//         return splitmix64(x + FIXED_RANDOM);
+//     }
+//     template<typename T, typename U>
+//     size_t operator()(const pair<T, U>& p) const {
+//         uint64_t h1 = operator()(static_cast<uint64_t>(p.first));
+//         uint64_t h2 = operator()(static_cast<uint64_t>(p.second));
+//         return h1 ^ (h2 << 1);
+//     }
+// };
+// struct hash_pair{
+//     size_t operator()(const std::pair<int, int>& p) const {
+//         return std::hash<int>()(p.first) ^ (std::hash<int>()(p.second) << 1);
+//     }
+// };
+// template<typename K, typename V>
+// using safe_umap = unordered_map<K, V, custom_hash>;
+// template<typename T>
+// using safe_uset = unordered_set<T, custom_hash>;
+
+// const int dx[4]={0,1,0,-1};
+// const int dy[4]={1,0,-1,0};
+
+// int customsearch(int tar, vector<int> &h,vector<bool> &temp){
+//     int ans=-1;
+//     int n=h.size();
+//     int l=0,r=n-1;
+//     while(l<=r){
+//         int m=l+(r-l)/2;
+//         if(h[m]<=tar && !temp[m]){
+//             ans=m;
+//             l=m+1;
+//         }
+//         else{
+//             r=m-1;
+//         }
+//     }
+//     return ans;
+// }
+
+// void solve() {
+//     int n,m; cin>>n>>m;
+//     vector<int> h(n);
+//     for(int i=0;i<n;i++){
+//        cin>>h[i];
+//     }
+//     sort(h.begin(),h.end());
+//     vector<bool> temp(n,false);
+//     vector<int> t(m);
+//     for(int i=0;i<m;i++){
+//         cin>>t[i];
+//         int temp2=customsearch(t[i],h,temp);
+//         if(temp2==-1 || temp2==n){
+//             cout<<-1<<endl;
+//             continue;
+//         }
+//         cout<<h[temp2]<<endl;
+//         temp[temp2]=true;
+//     }
+    
+// }
+
+// int32_t main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0); cout.tie(0);
+//     int tc = 1;
+//     //cin >> tc;
+//     for (int t = 1; t <= tc; t++) {
+//         // cout << "Case #" << t << ": ";
+//         solve();
+//     }
+// }
+//my first time implementing bianrysearch on my own but its giving tle lets use multiset
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define pb push_back
+#define umap unordered_map
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+// Custom hash for unordered_map/set
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    template<typename T, typename U>
+    size_t operator()(const pair<T, U>& p) const {
+        uint64_t h1 = operator()(static_cast<uint64_t>(p.first));
+        uint64_t h2 = operator()(static_cast<uint64_t>(p.second));
+        return h1 ^ (h2 << 1);
+    }
+};
+struct hash_pair{
+    size_t operator()(const std::pair<int, int>& p) const {
+        return std::hash<int>()(p.first) ^ (std::hash<int>()(p.second) << 1);
+    }
+};
+template<typename K, typename V>
+using safe_umap = unordered_map<K, V, custom_hash>;
+template<typename T>
+using safe_uset = unordered_set<T, custom_hash>;
+
+const int dx[4]={0,1,0,-1};
+const int dy[4]={1,0,-1,0};
+
+
+
+void solve() {
+    int n,m; cin>>n>>m;
+    //vector<int> p(n);
+    multiset<int> ms;
+    for(int i=0;i<n;i++){
+       //cin>>p[i];
+       int temp; cin>>temp;
+       ms.insert(temp);
+    }
+    while(m--){
+        int tar; cin>>tar;
+        auto it=ms.upper_bound(tar);
+        if(it==ms.begin()){
+            cout<<-1<<endl;
+        }
+        else{
+            it--;
+            cout<<*it<<endl;
+            ms.erase(it);
+        }
+
+    }
+
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
